868cd961b1d49f5dd6c2d8db30ae5c9d0f260975
Alifia Rahmah <alifiarahmah03@gmail.com>
2022-03-31 00:39:57 +0700

    Add print Kurang

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,49 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void mainMenu(){
-        System.out.println("15-Puzzle Solver");
+    public static void main(String[] args) {
+        // Main menu
+        System.out.println("===========================");
+        System.out.println("     15-Puzzle Solver      ");
+        System.out.println("===========================");
         System.out.println("[1] Generate random puzzle");
-        System.out.println("[2] Import puzzle (.txt in /test directory)");
+        System.out.println("[2] Import puzzle from /test directory");
         System.out.print("> ");
-
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        FifteenPuzzle board;
+        PuzzleBoard board;
         if (input.equals("1")) {
             // Generate random puzzle
-            board = new FifteenPuzzle();
+            board = new PuzzleBoard();
         } else {
             // Import puzzle
-            System.out.print("Enter file name: ");
+            System.out.print("Enter file name (with extension): ");
             input = scanner.nextLine();
-            board = new FifteenPuzzle(input);
+            board = new PuzzleBoard(input);
         }
-    }
 
-    public static void main(String[] args) {
-        FifteenPuzzle board = new FifteenPuzzle("tc1.txt");
+        // Cetak board
         board.printBoard();
-        if (board.isSolvable()) {
-            System.out.println("Solvable");
-
-            // Solve puzzle
-            // ? Buat export ke file biar niat banget kalo ada waktu
-        } else {
-            System.out.println("Not solvable");
+        // Cetak Kurang(i)
+        System.out.println("\nNilai Kurang(i)");
+        for(int i = 1; i < 16; i++) {
+            int[] loc = board.getValueIdx(i);
+            System.out.printf("%d %d\n", loc[0], loc[1]);
+            // System.out.printf("Kurang(%d): %d\n", i, board.Kurang(loc[0], loc[1]));
         }
+        // Cetak total isSolvable
+        System.out.printf("\nΣKurang + x = %d\n", board.SigmaKurang() + board.x());
+
+        // Solve puzzle
+        long startTime = System.currentTimeMillis();
+        if (board.isSolvable()) {
+            System.out.println("Puzzle Solvable.");
+            board.solve();
+        } else {
+            System.out.println("Puzzle not solvable.");
+        }
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println("Time Spent: " + duration + "ms");
     }
 }
