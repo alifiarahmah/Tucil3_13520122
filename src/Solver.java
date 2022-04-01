3e742d7cf989c0a93feaa20661e066d4b425b6a9
Alifia Rahmah <alifiarahmah03@gmail.com>
2022-04-02 02:35:55 +0700

    Change ArrayList to PriorityQueue

--- src/Solver.java
+++ src/Solver.java
@@ -1,10 +1,18 @@
-import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class NodeComparator implements Comparator<Node> {
+    @Override
+    public int compare(Node o1, Node o2) {
+        return Integer.compare(o1.getCost(), o2.getCost());
+    }
+}
 
 public class Solver {
 
     public static void solve(PuzzleBoard initialBoard) {
         // ArrayList<PuzzleBoard> expandedNode = new ArrayList<PuzzleBoard>();
-        ArrayList<Node> aliveNode = new ArrayList<>();
+        PriorityQueue<Node> aliveNode = new PriorityQueue<>(new NodeComparator());
         Node currentNode;
 
         // Initialize root node with initial board
@@ -14,40 +22,30 @@ public class Solver {
             // Ambil node yang sudah di expand, dan masukkan ke dalam queue
             if (currentNode.getBoard().getEmptyRowIdx() > 0) {
                 aliveNode.add(new Node(currentNode.getBoard().movedUp(), Move.UP, currentNode));
-                currentNode.setNextUp(aliveNode.get(aliveNode.size() - 1));
+                // currentNode.setNextUp(aliveNode.get(aliveNode.size() - 1));
             }
             if (currentNode.getBoard().getEmptyRowIdx() < 3) {
                 aliveNode.add(new Node(currentNode.getBoard().movedDown(), Move.DOWN, currentNode));
-                currentNode.setNextDown(aliveNode.get(aliveNode.size() - 1));
+                // currentNode.setNextDown(aliveNode.get(aliveNode.size() - 1));
             }
             if (currentNode.getBoard().getEmptyColIdx() > 0) {
                 aliveNode.add(new Node(currentNode.getBoard().movedLeft(), Move.LEFT, currentNode));
-                currentNode.setNextLeft(aliveNode.get(aliveNode.size() - 1));
+                // currentNode.setNextLeft(aliveNode.get(aliveNode.size() - 1));
             }
             if (currentNode.getBoard().getEmptyColIdx() < 3) {
                 aliveNode.add(new Node(currentNode.getBoard().movedRight(), Move.RIGHT, currentNode));
-                currentNode.setNextRight(aliveNode.get(aliveNode.size() - 1));
+                // currentNode.setNextRight(aliveNode.get(aliveNode.size() - 1));
             }
             System.out.println();
 
-            // Bandingkan cost dari semua node dalam expanded
-            // Board dengan cost paling kecil akan dijadikan current node
-            int minCost = aliveNode.get(0).getCost();
-            int selectedNodeIdx = 0;
-            for (int i = 1; i < aliveNode.size(); i++) {
-                if (aliveNode.get(i).getCost() < minCost) {
-                    minCost = aliveNode.get(i).getCost();
-                    selectedNodeIdx = i;
-                }
-            }
-
-            // Set current node menjadi node yang memiliki cost terkecil
-            currentNode = aliveNode.get(selectedNodeIdx);
-            aliveNode.remove(selectedNodeIdx);
+            currentNode = aliveNode.poll();
+            // currentNode.getBoard().printBoard();
+            // System.out.println("Move: " + currentNode.getMoveFromParent());
+            // System.out.println("Cost: " + currentNode.getCost());
 
             // Print board + langkah
-            System.out.print("Next move: ");
-            System.out.println(currentNode.getMoveFromParent());
+            assert currentNode != null;
+            System.out.println("Next move: " + currentNode.getMoveFromParent());
             currentNode.getBoard().printBoard();
 
         }
